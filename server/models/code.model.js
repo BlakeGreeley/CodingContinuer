e907c6588d6d167b3ddf78019b50765be6972b4e
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-10-03 14:37:48 -0700

    updated 10/3/22

--- server/models/code.model.js
+++ server/models/code.model.js
@@ -8,5 +8,26 @@ const CodeSchema = {
         maxLength: [128, 'topic cannot surpass 128 characters']
     }, 
 
-    
-}
+    language: {
+        type: String,
+        required: [true, 'coding language cannot be empty'], 
+        minLength: [2, 'language cannot be less than 2 characters'],
+        maxLength: [128, 'language cannot be more than 128 character']
+    },
+
+    code: {
+        type: String,
+        required: [true, 'code cannot be empty'], 
+        minLength: [3, 'code cannot be less than 3 characters'],
+        maxLength: [256, 'code cannot be more than 256 characters']
+    },
+
+    thoughts: {
+        type: String,
+        required: [false, 'thoughts on the code is not required but helpful'],
+        maxLength: [256, 'thoughts on your code cannot surpass 256 characters']
+    },
+
+};
+
+module.exports = mongoose.model('Code', CodeSchema);
