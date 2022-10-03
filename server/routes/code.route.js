cfb6c61064fed922cf3ad17aeadbd3f332eada18
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-10-03 14:40:41 -0700

    updated 10/3/22

--- server/routes/code.route.js
+++ server/routes/code.route.js
@@ -2,4 +2,8 @@ const codeController = require("../controllers/code.controller");
 
 module.exports = (app) => {
     app.post("/api/code", codeController.createNewCode);
+    app.get("/api/code", codeController.getAllCode);
+    app.get("/api/code/:id", codeController.getOneCode);
+    app.put("/api/code/:id", codeController.updateCode);
+    app.delete("/api/code/:id", codeController.deleteCode);
 }
